e2e0c30930ac08871dbae99ce170c17b6852e06b
jinlin Lin <jinlintan@sjtu.edu.cn>
2020-04-10 15:27:00 +0800

    网络位置信息

---
+++ app/src/main/java/com/xposed/hook/location/FLocation.java
@@ -0,0 +1,26 @@
+package com.xposed.hook.location;
+
+public class FLocation {
+    private String latitude, longitude;
+
+    public FLocation(String latitude, String Longitude){
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getLatitude (){
+        return latitude;
+    }
+
+    public void setLatitude(String latitude){
+        this.latitude = latitude;
+    }
+
+    public String getLongitude (){
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+}
